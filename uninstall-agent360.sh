44b4219afb77d3d970ebed0dedeb036c06be4602
Vladimir Ivanov <vladimir.ivanov@webpros.com>
2025-04-27 07:21:15 +0300

    added removal of /root/360monitoring folder and initialization via cPanel script

--- uninstall-agent360.sh
+++ uninstall-agent360.sh
@@ -92,11 +92,13 @@ else
   echo -e "\\e[33m[WARNING] The configuration file of the service agent360 is not found\n\t  Probably, it was removed earlier\\e[m"
 fi
 
-# if [[ -f /etc/agent360.ini ]] || [[ -f /etc/agent360-token.ini ]] ; then
-#   handle_cmd 'rm -f /etc/agent360*' "The 360 Monitoring configuration files have been deleted `echo`"
-# else
-#   echo -e "\\e[33m[WARNING] The 360 Monitoring configuration files are not found\n\t  Probably, they were removed earlier\\e[m"
-# fi
+if [[ -f /etc/agent360.ini ]] || [[ -f /etc/agent360-token.ini ]] || [[ -d /root/.360monitoring ]] ; then
+  handle_cmd 'rm -f /etc/agent360*' "The 360 Monitoring configuration files have been deleted `echo`"
+  handle_cmd 'rm -rf /root/.360monitoring' "The /root/.360monitoring folder have been deleted `echo`"
+  handle_cmd '/scripts/initialize_360monitoring' "Initializing cPanel 360 monitoring script /scripts/initialize_360monitoring `echo`"
+else
+  echo -e "\\e[33m[WARNING] The 360 Monitoring configuration files are not found\n\t  Probably, they were removed earlier\\e[m"
+fi
 if test -d /usr/local/cpanel/ 2>/dev/null;then
   echo -e "\\e[34m[NOTE] This is cPanel server. Skipping removal of /etc/agent360.ini file !\\e[m"
   echo -e "\\e[34m[NOTE] Removing file /etc/agent360-token.ini and /root/.360monitoring/ folder !\\e[m"
