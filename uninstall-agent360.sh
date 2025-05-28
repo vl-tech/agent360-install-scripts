12ebadf74d0b7e7d1797c73ce8b4019bdff69d9b
Vladimir Ivanov <vladimir.ivanov@webpros.com>
2025-05-28 12:56:02 +0300

    Fixed agent360-token.ini removal

--- uninstall-agent360.sh
+++ uninstall-agent360.sh
@@ -99,18 +99,18 @@ if [[ -f /etc/agent360.ini ]] || [[ -f /etc/agent360-token.ini ]] || [[ -d /root
 else
   echo -e "\\e[33m[WARNING] The 360 Monitoring configuration files are not found\n\t  Probably, they were removed earlier\\e[m"
 fi
-if test -d /usr/local/cpanel/ 2>/dev/null;then
-  echo -e "\\e[34m[NOTE] This is cPanel server. Skipping removal of /etc/agent360.ini file !\\e[m"
-  echo -e "\\e[34m[NOTE] Removing file /etc/agent360-token.ini and /root/.360monitoring/ folder !\\e[m"
-  handle_cmd 'rm -rf /etc/agent360-token.ini || rm -rf /root/.360monitoring/'
-else
-  echo -e "\\e[34m[NOTE] This is not cPanel server. Removing all agent360 ini files from /etc folder !\\e[m"
-  if [[ -f /etc/agent360.ini ]] || [[ -f /etc/agent360-token.ini ]] ; then
+#if test -d /usr/local/cpanel/ 2>/dev/null;then
+ # echo -e "\\e[34m[NOTE] This is cPanel server. Skipping removal of /etc/agent360.ini file !\\e[m"
+  #echo -e "\\e[34m[NOTE] Removing file /etc/agent360-token.ini and /root/.360monitoring/ folder !\\e[m"
+  #handle_cmd 'rm -rf /etc/agent360-token.ini || rm -rf /root/.360monitoring/'
+#else
+ # echo -e "\\e[34m[NOTE] This is not cPanel server. Removing all agent360 ini files from /etc folder !\\e[m"
+  if [[ -f /etc/agent360-token.ini ]] ; then
     handle_cmd 'rm -f /etc/agent360*' "The 360 Monitoring configuration files have been deleted `echo`"
   else
     echo -e "\\e[33m[WARNING] The 360 Monitoring configuration files are not found\n\t  Probably, they were removed earlier\\e[m"
   fi
-fi
+#fi
 if [[ -f /var/log/agent360.log ]] || [[ -f /var/log/agent360-install.log ]]; then
   echo
   read -r -p "Do you want to remove agent360 logs (y/n)? " choice
