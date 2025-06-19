c5644952d500eb21adf123aef9d1b2ce75a9f5e0
Vladimir Ivanov <vladimir.ivanov@webpros.com>
2025-06-19 14:18:45 +0300

    fixed requirements filepath

--- agent360.sh
+++ agent360.sh
@@ -598,10 +598,9 @@ touch $log_file
 #: MAIN BODY :#
 echo "> Getting the Linux distribution name and version..."
 get_os_release && get_os_version && echo -e "\\e[32m[SUCCESS] Found ${OS_RELEASE} ${OS_VERSION}\\e[m" || echo -e "\\e[31m[ERROR] Failed to find which Linux distribution is used"
-
+create_requirements_file
 if [ $skip_deps -eq 0 ]; then
   echo "> Installing the necessary packages..."
-  create_requirements_file &&
   get_installer &&
   check_agent360 &&
   check_cagefs_status_and_cofiguration &&
